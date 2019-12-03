f847cae9cbfcb5b3f5fcb7e75bcd83acb17ceb7b
StarsDeath2938 <starsdeath2938@gmail.com>
2019-12-03 20:09:40 +0800

    Mod : Top-Nav

--- js/top-nav.js
+++ js/top-nav.js
@@ -1,12 +1,19 @@
-$(document).ready(function () {
-    $(document).scroll(function () {
+$(function () {
 
-        let scroll = $("nav").scrollTop();
+    let nav = $(".topNavList");
+    let navOffset = $(".topNavList").offset();
 
-        if (scroll == 0) {
-            alert("test");
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() > navOffset.top - 40) {
+            nav.addClass('top-nav-list-hidden');
+        }
+
+        else {
+            nav.removeClass('top-nav-list-hidden');
         }
 
     });
 
-});
+})
+
