7ea6ba17d90c928074f6e1ab05105e9ec7ee3ab1
StarsDeath2938 <starsdeath2938@gmail.com>
2020-01-06 19:43:19 +0800

    Del : Mobile Projects

--- Project06/index.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $(".sticky-nav").children("div").mouseenter(function () {
-        $(this).children("div").css({ "display": "block" });
-    });
-
-    $(".sticky-nav").children("div").mouseleave(function () {
-        $(this).children("div").css({ "display": "none" });
-    });
-});
